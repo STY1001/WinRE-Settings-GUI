2a1b4634a55a7743477ebd2c88f816fcdf84299a
STY1001 <57889852+STY1001@users.noreply.github.com>
2022-10-09 17:45:49 +0200

    Done

--- WinRE Settings GUI/Main.cs
+++ WinRE Settings GUI/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Net;
@@ -14,7 +15,7 @@ namespace WinRE_Settings_GUI
 {
     public partial class Main : Form
     {
-        //Set dark mode title bar
+        //Set dark mode title bar and bypass wow64
 
         [DllImport("DwmApi")]
         private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, int[] attrValue, int attrSize);
@@ -25,7 +26,15 @@ namespace WinRE_Settings_GUI
             DwmSetWindowAttribute(Handle, 20, new[] { 1 }, 4);
             DwmSetWindowAttribute(Handle, 35, new[] { 1 }, 4);
             DwmSetWindowAttribute(Handle, 38, new[] { 1 }, 4);
+            IntPtr wow64Value = IntPtr.Zero;
+            Wow64DisableWow64FsRedirection(ref wow64Value);
         }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Wow64DisableWow64FsRedirection(ref IntPtr ptr);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool Wow64RevertWow64FsRedirection(IntPtr ptr);
 
         public Main()
         {
@@ -48,6 +57,14 @@ namespace WinRE_Settings_GUI
             {
                 web.DownloadFile("https://dl.dropbox.com/s/lahofrvpejlclkx/Winre.wim", "C:\\Windows\\System32\\Recovery\\WinRE.wim");
             }
+
+            var p = new Process();
+            p.StartInfo.FileName = "regentc";
+            p.StartInfo.Arguments = "/setreimage /path C:\\Windows\\System32\\Recovery";
+            p.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
+            p.WaitForExit();
+
+            System.Diagnostics.Process.Start("reagentc.exe", "/enable");
         }
     }
 }
